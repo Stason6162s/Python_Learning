4d87c1f441ded2340b5a03489a8208a861a7c531
CEU857573 <CEU857573@EU-ST-05032>
2019-07-10 11:08:13 +0300

    190710_1106: Add method of documentation to hints module.

--- Expirements/hints.py
+++ Expirements/hints.py
@@ -8,4 +8,21 @@ print(place_holders_string)
 format_string = "My name is {0}, i am {1} years".format(name, age)
 print(format_string)
 
+
+# Documentation of code
+
+def some_function(any_type, string='', integer=0):
+    """
+    The function print string from own arguments
+    :param any_type: the argument may be any type
+    :param string: the argument should be string, and default value - empty string
+    :param integer: the argument should be string, and default value - zero
+    :return: Arguments string
+    """
+    return_string = f"My name is {any_type}, i am {str(integer)} {string}"
+    return return_string
+
+
 # Next...
+if __name__ == '__main__':
+    print(some_function('Stas', 'years', 32))
