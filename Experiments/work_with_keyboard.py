0e86d8b6cc082f3776370297f259946e94f80b8f
CEU857573 <CEU857573@EU-ST-05032>
2019-11-15 10:02:02 +0300

    191115_0921: The module 'work_with_keyboard' has been added.

--- Experiments/work_with_keyboard.py
+++ Experiments/work_with_keyboard.py
@@ -1,15 +1,28 @@
 from msvcrt import getch
 
+import keyboard
+
 
 def on_board_func():
     while True:
         key = ord(getch())
         if key == 27:
-            print(f'Pushed the key with number {key}')
             break
         else:
-            print(f'Pushed the key with number {key}')
+            print(f'Pushed the {chr(key)} keys with number {key}')
+
+
+def using_keyboard():
+    while True:
+        key = keyboard.get_hotkey_name()
+        if key == 'q':
+            print(f'Pushed the {keyboard.get_hotkey_name()}')
+            break
+        else:
+            if key != '':
+                print(f'Pushed the {key} button')
 
 
 if __name__ == '__main__':
     on_board_func()
+    # using_keyboard()
